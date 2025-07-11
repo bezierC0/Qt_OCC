9ca8ba02541e67694f7179d12250eb224eadaec9
harry <kimisnan@hotmail.com>
2025-07-11 09:38:57 +0900

    vscode debug

---
+++ .vscode/.prettier.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        tabWidth: 2
+      }
+    },
+    {
+      files: "*.js",
+      options: {
+        tabWidth: 2
+      }
+    },
+    {
+      files: "*.ts",
+      options: {
+        tabWidth: 2
+      }
+    }
+  ]
+};
